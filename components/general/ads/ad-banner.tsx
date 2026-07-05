cef445848a66e9734d1987bf9be1f44f9941ad27
codewithtabish <kashisultan099@gmail.com>
2026-07-05 16:51:31 +0500

    cgnaged banner id

--- components/general/ads/ad-banner.tsx
+++ components/general/ads/ad-banner.tsx
@@ -1,33 +1,32 @@
 'use client';
 
-import { ADMOB_CONFIG } from '@/lib/admob';
 import Script from 'next/script';
 
 export default function AdBanner() {
   return (
     <>
-      {/* Load Google AdMob Script */}
+      {/* Load AdMob Script */}
       <Script
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
         strategy="afterInteractive"
       />
 
-      {/* Bottom Banner Ad */}
-      <div className="fixed bottom-0 left-0 right-0 z-50 bg-black border-t border-zinc-700 py-2 shadow-lg">
-        <div className="max-w-md mx-auto px-4 text-center">
+      {/* Bottom Banner */}
+      <div className="fixed bottom-0 left-0 right-0 z-50 bg-black border-t border-zinc-700 py-2">
+        <div className="max-w-md mx-auto px-4">
           <ins
             className="adsbygoogle"
             style={{ display: "block" }}
-            data-ad-client={ADMOB_CONFIG.PUBLISHER_ID}
-            data-ad-slot={ADMOB_CONFIG.BANNER_ID}
+            data-ad-client="ca-pub-3940256099942544"   // Test Publisher ID
+            data-ad-slot="6300978111"                  // Test Banner ID
             data-ad-format="auto"
             data-full-width-responsive="true"
           />
         </div>
       </div>
 
-      {/* Initialize Ads */}
+      {/* Initialize the ad */}
       <Script id="ad-init" strategy="afterInteractive">
         {`(adsbygoogle = window.adsbygoogle || []).push({});`}
       </Script>
